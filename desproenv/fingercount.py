e0a2ebbdf628a71e8121df69d607f35158d09c1f
riefkyarif <riefkyar.ed@protonmail.com>
2021-10-13 00:13:56 +0700

    add some stuffs

--- desproenv/fingercount.py
+++ desproenv/fingercount.py
@@ -1,1 +1,44 @@
-print ("test")
+import cv2
+import handtracking as htm
+import arduino as ac
+
+wCam, hCam = 640, 480
+val = 0
+totalFingers = 0
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+detector = htm.handDetector(detectionCon=0.75)
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        fingers = []
+        #Jempol
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        #4 Jari
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        totalFingers: int = fingers.count(1)
+
+        ac.ValueOfLight.ledLight(totalFingers)
+
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0,), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 365), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 15)
+        cv2.putText(img, "Gestur", (53, 410), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
